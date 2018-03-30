4d07d6dd31d542294763b6cf6c8c667c9dfd9aed
Kiana Kim <kianamkim@gmail.com>
2018-03-29 23:13:47 -0400

    Add blank main .ino file in folder

---
+++ RBE2002-Final-Project-Main/RBE2002-Final-Project-Main.ino
@@ -0,0 +1,12 @@
+// RBE 2002 Main Final Project Code
+// Team 17
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
