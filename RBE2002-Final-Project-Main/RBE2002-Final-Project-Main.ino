b24c3d2a94d75beedf7208fc982ae1558f5cc31d
Kiana Kim <kianamkim@gmail.com>
2018-03-29 23:26:08 -0400

    Added drive motor pins and flame sensor pin

--- RBE2002-Final-Project-Main/RBE2002-Final-Project-Main.ino
+++ RBE2002-Final-Project-Main/RBE2002-Final-Project-Main.ino
@@ -1,12 +1,27 @@
+// Team 17 
 // RBE 2002 Main Final Project Code
-// Team 17
+
+// Drive Motor Pins (MC33926 Pololu Driver)
+const int rightDriveFWDPin = 8;
+const int rightDriveREVPin = 9;
+const int leftDriveFWDPin = 7;
+const int leftDriveREVPin = 6;
+
+// Sensor Pins
+const int flameSensorPin = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(rightDriveFWDPin, OUTPUT);
+  pinMode(rightDriveREVPin, OUTPUT);
+  
+  pinMode(leftDriveFWDPin, OUTPUT);
+  pinMode(leftDriveREVPin, OUTPUT);
 
+  pinMode(flameSensorPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
