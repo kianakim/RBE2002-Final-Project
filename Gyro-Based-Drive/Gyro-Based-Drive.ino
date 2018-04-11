a0991b1ee9daf8096372beebb7f25f10ec9118c1
Kiana Kim <kianamkim@gmail.com>
2018-04-10 20:34:57 -0400

    added gyro functions from lab4, not completed

--- Gyro-Based-Drive/Gyro-Based-Drive.ino
+++ Gyro-Based-Drive/Gyro-Based-Drive.ino
@@ -1,11 +1,22 @@
 // Turn Drive with IMU
-// Team 17
+// uses some functions from lab 4 compliment file
 
 #include <Wire.h>
 #include <L3G.h>
 #include <LSM303.h>
 L3G gyro;
 LSM303 accel;
+
+// gyro variables
+float G_Dt=0.020;    // Integration time (DCM algorithm)
+double const G_gain = .00875; // gyros gain factor for 250deg/sec
+double gyro_x;
+double gyro_y;
+double gyro_z;
+
+double gerr_x;
+double gerr_y;
+double gerr_z;
 
 
 void setup() {
@@ -25,14 +36,46 @@ void loop() {
 }
 
 // gyro calibration "zero"
+// sample 200 readings and take the average to get calibration offset
 void gyroZero() {
-  for(int i = 0; i < 200; i++) {
+  for (int i = 0; i < 200; i++) {
     gyro.read();
-    
+
+    gerr_x += gyro.g.x;
+    gerr_y += gyro.g.y;
+    gerr_z += gyro.g.z;
+
+    delay(20);
   }
+
+  gerr_x = gerr_x / 200;
+  gerr_y = gerr_y / 200;
+  gerr_z = gerr_z / 200;
 }
+
 // read gyro
+int gyroRead() {
+  gyro.read();
+  gyro_x = (double) (gyro.g.x - gerr_x) * G_gain;
+  gyro_y = (double) (gyro.g.y - gerr_y) * G_gain;
+  gyro_z = (double) (gyro.g.z - gerr_z) * G_gain;
+
+  gyro_x = gyro_x * G_Dt; // Multiply the angular rate by the time interval
+  gyro_y = gyro_y * G_Dt;
+  gyro_z = gyro_z * G_Dt;
+
+  // add stuff
+}
+
 
 // print gyro
-
+// from lab4 file
+void printGyro(){
+  Serial.print(" GX: ");
+  Serial.print(gyro_x);
+  Serial.print(" GY: ");
+  Serial.print(gyro_y);
+  Serial.print(" GZ: ");
+  Serial.print(gyro_z);
+}
 
