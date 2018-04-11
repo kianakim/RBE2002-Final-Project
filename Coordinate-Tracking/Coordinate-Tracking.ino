7e6c6f08e725bb2f834b0be6e8d0799dee56bd59
Kiana Kim <kianamkim@gmail.com>
2018-04-11 10:51:07 -0400

    dist track w/ accel is bad, added new project

---
+++ Coordinate-Tracking/Coordinate-Tracking.ino
@@ -0,0 +1,16 @@
+// Storing distance data
+// store drive forward values and direction to keep track of position
+
+// coordinate variables
+double candle_x = 0;
+double candle_y = 0;
+double candle_z = 0;
+
+void setup() {
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
