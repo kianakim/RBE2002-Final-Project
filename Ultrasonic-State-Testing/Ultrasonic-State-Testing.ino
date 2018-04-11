ae0bbdfa1095279b7681cc0638ea2849f7043253
Kiana Kim <kianamkim@gmail.com>
2018-04-11 11:52:29 -0400

    finished implementing example code

--- Ultrasonic-State-Testing/Ultrasonic-State-Testing.ino
+++ Ultrasonic-State-Testing/Ultrasonic-State-Testing.ino
@@ -8,12 +8,12 @@
 
 // sensor object array
 NewPing sensors[SONAR_NUM] = {
-  NewPing(1, 2, MAX_DISTANCE);
-  NewPing(3, 4, MAX_DISTANCE);
-  NewPing(5, 6, MAX_DISTANCE);
-  NewPing(7, 8, MAX_DISTANCE);
-  NewPing(9, 10, MAX_DISTANCE);
-}
+  NewPing(1, 2, MAX_DISTANCE),
+  NewPing(3, 4, MAX_DISTANCE),
+  NewPing(5, 6, MAX_DISTANCE),
+  NewPing(7, 8, MAX_DISTANCE),
+  NewPing(9, 10, MAX_DISTANCE),
+};
 
 // which sensor is active
 unsigned int active_sensor = 0;
@@ -24,7 +24,9 @@ unsigned int dist[SONAR_NUM];
 
 void setup() {
   Serial.begin(115200);
-
+  pingTimer[0] = millis() + 75; // First ping start in ms.
+  for (uint8_t i = 1; i < SONAR_NUM; i++)
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
 }
 
 void loop() {
@@ -32,8 +34,40 @@ void loop() {
 
 }
 
-// If ping echo, set distance to array.
-void echoCheck() { 
-  if (sensors[active_sensor].check_timer())
-    dist[active_sensor] = sensors[active_sensor].ping_result / US_ROUNDTRIP_CM;
+void readAllUltrasonic() {
+  for (int i = 0; i < SONAR_NUM; i++) {
+    
+    // add to ping timer for next check
+    if (millis() >= pingTimer[i]) {
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;
+    }
+    
+    // end condition check
+    if (i == 0 && active_sensor == SONAR_NUM - 1) {
+      printReadings();
+    }
+
+    // stop timer, reset var
+    sensors[active_sensor].timer_stop();
+    active_sensor = i;
+    dist[active_sensor] = 0;
+    sensors[active_sensor].ping_timer(echoCheck);
+  }
 }
+
+// If ping echo, set distance to array.
+void echoCheck() {
+  if (sensors[active_sensor].check_timer())
+    dist[active_sensor] = sensors[active_sensor].ping_result / US_ROUNDTRIP_CM; // conversion factor in lib
+}
+
+void printReadings() {
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(dist[i]);
+    Serial.print("cm ");
+  }
+  Serial.println();
+}
+
