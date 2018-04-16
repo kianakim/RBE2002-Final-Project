e72177d590075f70a3c16c09e3bb3ea1e69f1104
Kiana Kim <kianamkim@gmail.com>
2018-04-16 17:40:37 -0400

    state things are fucked rn

--- Ultrasonic-State-Testing/Ultrasonic-State-Testing.ino
+++ Ultrasonic-State-Testing/Ultrasonic-State-Testing.ino
@@ -6,13 +6,22 @@
 #define MAX_DISTANCE 200
 #define PING_INTERVAL 33 // just example
 
+#define FRONT_RIGHT 0
+#define FRONT 1
+#define BACK_LEFT 2
+#define FRONT_LEFT 3
+#define BACK_RIGHT 4
+
+// states
+static enum ultraState {forward, frontWall, noWall, checkDir, blockedIn} ultraState;
+
 // sensor object array
 NewPing sensors[SONAR_NUM] = {
-  NewPing(12, 11, MAX_DISTANCE),  // back left
-  NewPing(9, 8, MAX_DISTANCE),  // back right
-  NewPing(6, 5, MAX_DISTANCE)  // front right
-  //  NewPing(0, 1, MAX_DISTANCE),  // front left
-  //  NewPing(2, 3, MAX_DISTANCE), // front
+  NewPing(12, 11, MAX_DISTANCE),  // front right
+  NewPing(10, 9, MAX_DISTANCE),  // front
+  NewPing(8, 7, MAX_DISTANCE)  // back left
+  NewPing(6, 5, MAX_DISTANCE),  // front left
+  NewPing(3, 2, MAX_DISTANCE), // back right
 };
 
 // sensor variables
@@ -29,11 +38,46 @@ void setup() {
   // sets ping timers
   for (uint8_t i = 1; i < SONAR_NUM; i++)
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+
+  ultraState = forward;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   readAllUltrasonic();
+  
+//  switch (ultraState) {
+//    case forward:
+//      // drive forward
+//      Serial.println("forward");
+//      analogWrite(1, 180); // dummy drive cmd
+//      break;
+//
+//    case frontWall:
+//      // stop
+//      Serial.println("frontWall");
+//      analogWrite(1, 0);
+//      break; // could simplify w/ no wall
+//
+//    case noWall:
+//      // stop
+//      Serial.println("noWall");
+//      analogWrite(1, 0);
+//      break;
+//
+//    case checkDir:
+//      // look at left and right sensor to pick direction
+//      Serial.println("checkDir");
+//      break;
+//
+//    case blockedIn:
+//      // drive backwards or w/e do 180
+//      Serial.println("blockedIn");
+//      break;
+//
+//    default:
+//      break;
+//  }
 }
 
 void readAllUltrasonic() {
@@ -45,7 +89,8 @@ void readAllUltrasonic() {
 
       // end condition check
       if (active_sensor == SONAR_NUM - 1)
-        printReadings();
+        //      printReadings();
+        decisionMaking();
 
       // update variables
       sensors[active_sensor].timer_stop();
@@ -75,11 +120,21 @@ void printReadings() {
   Serial.println();
 }
 
-// garbage test code
-//  active_sensor = 0;
-//  sensors[active_sensor].ping_timer(echoCheck);
-//  Serial.print("sensor ");
-//  Serial.print(active_sensor);
-//  Serial.print(": ");
-//  Serial.println(sensors[active_sensor].ping_result / 58.2);
-
+void decisionMaking() {
+  if (dist[FRONT] < 20) {
+    Serial.println("front wall");
+    ultraState = frontWall;
+  }
+  else {
+    Serial.println("forward");
+    ultraState = forward;
+  }
+  
+  if (dist[FRONT_LEFT] > 20) {
+    Serial.println("no right wall");
+    ultraState = noWall;
+  }
+  else {
+    ultraState = forward;
+  }
+}
