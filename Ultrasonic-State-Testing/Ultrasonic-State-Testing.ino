82137d60255ddbbe71926ddff7dbb301cee1f4b9
Kiana Kim <kianamkim@gmail.com>
2018-04-16 14:28:21 -0400

    working ultrasonic proj w/ 3 sensors

--- Ultrasonic-State-Testing/Ultrasonic-State-Testing.ino
+++ Ultrasonic-State-Testing/Ultrasonic-State-Testing.ino
@@ -2,29 +2,31 @@
 // Based off of NewPing 15 Sensor Example: https://playground.arduino.cc/Code/NewPing
 #include <NewPing.h>
 
-#define SONAR_NUM 5
+#define SONAR_NUM 3
 #define MAX_DISTANCE 200
 #define PING_INTERVAL 33 // just example
 
 // sensor object array
 NewPing sensors[SONAR_NUM] = {
-  NewPing(4, 5, MAX_DISTANCE),  // back left
-  NewPing(6, 7, MAX_DISTANCE),  // back right
-  NewPing(15, 14, MAX_DISTANCE),  // front right
-  NewPing(0, 1, MAX_DISTANCE),  // front left
-  NewPing(2, 3, MAX_DISTANCE), // front
+  NewPing(12, 11, MAX_DISTANCE),  // back left
+  NewPing(9, 8, MAX_DISTANCE),  // back right
+  NewPing(6, 5, MAX_DISTANCE)  // front right
+  //  NewPing(0, 1, MAX_DISTANCE),  // front left
+  //  NewPing(2, 3, MAX_DISTANCE), // front
 };
 
-// which sensor is active
+// sensor variables
 unsigned int active_sensor = 0;
 
 // array to store when each sensor pings and distances
 unsigned long pingTimer[SONAR_NUM];
-unsigned int dist[SONAR_NUM];
+double dist[SONAR_NUM];
 
 void setup() {
   Serial.begin(115200);
   pingTimer[0] = millis() + 75; // First ping start in ms.
+
+  // sets ping timers
   for (uint8_t i = 1; i < SONAR_NUM; i++)
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
 }
@@ -36,29 +38,30 @@ void loop() {
 
 void readAllUltrasonic() {
   for (int i = 0; i < SONAR_NUM; i++) {
-    
-    // add to ping timer for next check
+
+    // ready to take reading
     if (millis() >= pingTimer[i]) {
       pingTimer[i] += PING_INTERVAL * SONAR_NUM;
-    }
-    
-    // end condition check
-    if (i == 0 && active_sensor == SONAR_NUM - 1) {
-      printReadings();
-    }
 
-    // stop timer, reset var
-    sensors[active_sensor].timer_stop();
-    active_sensor = i;
-    dist[active_sensor] = 0;
-    sensors[active_sensor].ping_timer(echoCheck);
+      // end condition check
+      if (active_sensor == SONAR_NUM - 1)
+        printReadings();
+
+      // update variables
+      sensors[active_sensor].timer_stop();
+      active_sensor = i;
+      dist[active_sensor] = 0;
+      sensors[active_sensor].ping_timer(echoCheck);
+    }
   }
+
+  // rest of code
 }
 
-// If ping echo, set distance to array.
+// user function: reads distance from active sensor
 void echoCheck() {
   if (sensors[active_sensor].check_timer())
-    dist[active_sensor] = sensors[active_sensor].ping_result / US_ROUNDTRIP_CM; // conversion factor in lib
+    dist[active_sensor] = sensors[active_sensor].ping_result / 58.2; // convert uS to cm
 }
 
 // prints readings for testing
@@ -72,4 +75,11 @@ void printReadings() {
   Serial.println();
 }
 
+// garbage test code
+//  active_sensor = 0;
+//  sensors[active_sensor].ping_timer(echoCheck);
+//  Serial.print("sensor ");
+//  Serial.print(active_sensor);
+//  Serial.print(": ");
+//  Serial.println(sensors[active_sensor].ping_result / 58.2);
 
