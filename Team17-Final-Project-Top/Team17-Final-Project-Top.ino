d7bdcbd9e729c92ddbff8d799420a98adb3ffdbc
Kiana Kim <kianamkim@gmail.com>
2018-04-22 19:44:43 -0400

    added pin definitions for ultrasonic, cliff, flame sensor

--- Team17-Final-Project-Top/Team17-Final-Project-Top.ino
+++ Team17-Final-Project-Top/Team17-Final-Project-Top.ino
@@ -2,12 +2,38 @@
 // Team 17
 // Top Arduino Version
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <NewPing.h>
 
+// Ultrasonic Sensors
+#define SONAR_NUM 3
+#define MAX_DISTANCE 200
+#define PING_INTERVAL 33
+
+#define FRONT 0
+#define BACK_LEFT 1
+#define FRONT_LEFT 2
+
+NewPing sensors[SONAR_NUM] = {
+  NewPing(10, 9, MAX_DISTANCE),  // front
+  NewPing(8, 7, MAX_DISTANCE),  // back left
+  NewPing(6, 5, MAX_DISTANCE)  // front left
+};
+
+unsigned int active_sensor = 0;
+unsigned long pingTimer[SONAR_NUM];
+double dist_cm[SONAR_NUM];
+
+// Cliff Sensor
+const byte cliffPin = 1;
+
+// Flame Sensor (analog)
+const byte flamePin = 0;
+
+void setup() {
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
