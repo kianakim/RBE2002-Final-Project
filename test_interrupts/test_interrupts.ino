b7f898fc891fee43e1d70ad040b51c5086887e16
Kiana Kim <kianamkim@gmail.com>
2018-04-27 19:17:35 -0400

    added code to test interrupt pins report serial print state

---
+++ test_interrupts/test_interrupts.ino
@@ -0,0 +1,58 @@
+int rstate = 0;
+
+const byte flameRXPin = 2;
+const byte frontRXPin = 18;
+const byte sideWallRXPin = 19;
+const byte cliffRXPin = 3;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("STARTING SETUP");
+
+  attachInterrupt(digitalPinToInterrupt(flameRXPin), flameInt, RISING);
+  attachInterrupt(digitalPinToInterrupt(frontRXPin), frontWallInt, RISING);
+  attachInterrupt(digitalPinToInterrupt(sideWallRXPin), noSideWallInt, RISING);
+  attachInterrupt(digitalPinToInterrupt(cliffRXPin), cliffInt, RISING);
+}
+
+void loop() {
+  switch (rstate) {
+    case 0:
+      Serial.println("INITIAL");
+      break;
+
+    case 1:
+      Serial.println("FLAME");
+      break;
+
+    case 2:
+      Serial.println("FRONT WALL");
+      break;
+
+    case 3:
+      Serial.println("NO SIDE WALL");
+      break;
+
+    case 4:
+      Serial.println("CLIFF");
+      break;
+  }
+  delay(100);
+}
+
+void flameInt() {
+  rstate = 1;
+}
+
+void frontWallInt() {
+  rstate = 2;
+}
+
+void noSideWallInt() {
+  rstate = 3;
+}
+
+void cliffInt() {
+  rstate = 4;
+}
+
