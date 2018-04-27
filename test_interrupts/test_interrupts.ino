4c7c5862dccb570132be3b20284a331d27a6da90
Kiana Kim <kianamkim@gmail.com>
2018-04-27 19:35:19 -0400

    set pins to input_pullup

--- test_interrupts/test_interrupts.ino
+++ test_interrupts/test_interrupts.ino
@@ -9,6 +9,11 @@ void setup() {
   Serial.begin(9600);
   Serial.println("STARTING SETUP");
 
+  pinMode(flameRXPin, INPUT_PULLUP);
+  pinMode(frontRXPin, INPUT_PULLUP);
+  pinMode(sideWallRXPin, INPUT_PULLUP);
+  pinMode(cliffRXPin, INPUT_PULLUP);
+  
   attachInterrupt(digitalPinToInterrupt(flameRXPin), flameInt, RISING);
   attachInterrupt(digitalPinToInterrupt(frontRXPin), frontWallInt, RISING);
   attachInterrupt(digitalPinToInterrupt(sideWallRXPin), noSideWallInt, RISING);
